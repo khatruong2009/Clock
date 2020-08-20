891f1f17d971ae382d8602f3858f351a545c6575
Kha Truong <khatruong2009@gmail.com>
2020-08-20 09:04:58 -0400

    added zeros before numbers if they were below 10

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ function clock() {
   var minute = date.getMinutes();
   var second = date.getSeconds();
 
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+
+  if (second < 10) {
+    second = "0" + second;
+  }
+
   if (hour > 12) {
     var newHour = hour%12;
     if (minute < 10) {
-      $(".clock h2").text(newHour + ":0" + minute + ":" + second + " PM");  
+      $(".clock h2").text(newHour + ":" + minute + ":" + second + " PM");  
     }
     if (second < 10) {
-      $(".clock h2").text(newHour + ":" + minute + ":0" + second + " PM");    
+      $(".clock h2").text(newHour + ":" + minute + ":" + second + " PM");    
     }
     if (minute < 10 && second < 10) {
-      $(".clock h2").text(newHour + ":0" + minute + ":0" + second + " PM");  
+      $(".clock h2").text(newHour + ":" + minute + ":" + second + " PM");  
     }
     $(".clock h2").text(newHour + ":" + minute + ":" + second + " PM")
   } else {
     if (minute < 10) {
-      $(".clock h2").text(hour + ":0" + minute + ":" + second + " AM");    
+      $(".clock h2").text(hour + ":" + minute + ":" + second + " AM");    
     } 
     if (second < 10) {
-      $(".clock h2").text(hour + ":" + minute + ":0" + second + " AM");
+      $(".clock h2").text(hour + ":" + minute + ":" + second + " AM");
     } 
     if (minute < 10 && second < 10) {
-      $(".clock h2").text(hour + ":0" + minute + ":0" + second + " AM");
+      $(".clock h2").text(hour + ":" + minute + ":" + second + " AM");
     }
     $(".clock h2").text(hour + ":" + minute + ":" + second + " AM");
   }
